dd7b2f965a7ebd79f39a77926260e02cb30b1d74
paddlefish <rohan.port@gmail.com>
2016-09-30 17:09:24 +1000

    Renamed

--- core/src/com/mygdx/game/skirmish/gameplay/TimeDelayedOperation.java
+++ core/src/com/mygdx/game/skirmish/gameplay/TimeDelayedOperation.java
@@ -3,7 +3,7 @@ package com.mygdx.game.skirmish.gameplay;
 /**
  * Created by paddlefish on 30-Sep-16.
  */
-public interface TimeDelayedOperatoin {
+public interface TimeDelayedOperation {
     float getTotalTime();
     float getRemainingTime();
     void performOperation();
